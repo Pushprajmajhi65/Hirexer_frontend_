cc1a9e6d4332faee830f87600be0095b36754a5b
Pushprajmajhi65 <pushprajmajhi65@gmail.com>
2025-03-29 08:57:46 +0545

    added page routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Loader from "./components/shared/Loader";
 import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { PublicRoute } from "./components/auth/PublicRoute";
 import { AuthProvider } from "./context/AuthContext";
+import { WorkspaceProvider } from "./context/WorkspaceContext";
 const SuccessfulPasswordReset = lazy(() =>
   import("./pages/SuccessfulPasswordReset")
 );
@@ -17,7 +18,7 @@ const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
 const OTP = lazy(() => import("./pages/SignupOTP"));
 const ResetPassword = lazy(() => import("./pages/ResetPassword"));
-const NewPassword = lazy(() => import("./pages/NewPassword"));
+/* const NewPassword = lazy(() => import("./pages/NewPassword")); */
 const ResetPasswordOTP = lazy(() => import("./pages/ResetPasswordOTP"));
 const Overview = lazy(() => import("./pages/Overview"));
 const WorkSpaceSetupDone = lazy(() => import("./pages/WorkspaceSetupdone"));
@@ -28,144 +29,127 @@ const Applications = lazy(() => import("./pages/Applications"));
 const Profile = lazy(() => import("./pages/Profile"));
 const Employee = lazy(() => import("./pages/Employee"));
 const Meeting = lazy(() => import("./pages/Meeting"));
+const Feed = lazy(() => import("./pages/Feed"));
+const RTC = lazy(() => import("./pages/RTC"));
 
 const App = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 10 * 1000,
       },
     },
   });
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Suspense fallback={<Loader />}>
-          <BrowserRouter>
-            <ScrollToTop />
-            <Toaster
-              position="top-right"
-              gutter={12}
-              containerStyle={{ margin: "8px" }}
-              toastOptions={{
-                success: { duration: 5000 },
-                error: { duration: 5000 },
-                style: {
-                  fontSize: "16px",
-                  maxWidth: "500px",
-                  padding: "16px 24px",
-                },
-              }}
-            />
-            <Routes>
-              <Route
-                path="/login"
-                element={
-                  <PublicRoute>
-                    <Login />
-                  </PublicRoute>
-                }
+        <WorkspaceProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Suspense fallback={<Loader />}>
+            <BrowserRouter>
+              <ScrollToTop />
+              <Toaster
+                position="top-right"
+                gutter={12}
+                containerStyle={{ margin: "8px" }}
+                toastOptions={{
+                  success: { duration: 5000 },
+                  error: { duration: 5000 },
+                  style: {
+                    fontSize: "16px",
+                    maxWidth: "500px",
+                    padding: "16px 24px",
+                  },
+                }}
               />
-              <Route
-                path="/signup"
-                element={
-                  <PublicRoute>
-                    <Signup />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/otp"
-                element={
-                  <PublicRoute>
-                    <OTP />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/resetPassword"
-                element={
-                  <PublicRoute>
-                    <ResetPassword />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/resetpasswordotp"
-                element={
-                  <PublicRoute>
-                    <ResetPasswordOTP />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/newpassword/:token"
-                element={
-                  <PublicRoute>
-                    <NewPassword />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/successfulreset"
-                element={
-                  <PublicRoute>
-                    <SuccessfulPasswordReset />
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/createworkspace"
-                element={
-                  <ProtectedRoute>
-                    <CreateWorkspace />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/onboarding"
-                element={
-                  <ProtectedRoute>
-                    <OnBoarding />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/workspacesetupdone"
-                element={
-                  <ProtectedRoute>
-                    <WorkSpaceSetupDone />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/workspaceinvite"
-                element={
-                  <ProtectedRoute>
-                    <WorkspaceInvitation />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                element={
-                  <ProtectedRoute>
-                    <Layout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route path="/overview" element={<Overview />} />
-                <Route index element={<Overview />} />
-
-                <Route path="/applications" element={<Applications />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/employee" element={<Employee />} />
-                <Route path="/meetings" element={<Meeting />} />
-              </Route>
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
+              <Routes>
+                <Route
+                  path="/login"
+                  element={
+                    <PublicRoute>
+                      <Login />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/signup"
+                  element={
+                    <PublicRoute>
+                      <Signup />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/otp"
+                  element={
+                    <PublicRoute>
+                      <OTP />
+                    </PublicRoute>
+                  }
+                />
+                <Route path="/resetPassword" element={<ResetPassword />} />
+                <Route
+                  path="/resetpasswordotp/:email"
+                  element={<ResetPasswordOTP />}
+                />
+                {/*  <Route path="/newpassword/:token" element={<NewPassword />} /> */}
+                <Route
+                  path="/successfulreset"
+                  element={<SuccessfulPasswordReset />}
+                />
+                <Route
+                  path="/createworkspace"
+                  element={
+                    <ProtectedRoute>
+                      <CreateWorkspace />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/onboarding"
+                  element={
+                    <ProtectedRoute>
+                      <OnBoarding />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/workspacesetupdone"
+                  element={
+                    <ProtectedRoute>
+                      <WorkSpaceSetupDone />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/workspaceinvite/:id"
+                  element={
+                    <ProtectedRoute>
+                      <WorkspaceInvitation />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  element={
+                    <ProtectedRoute>
+                      <Layout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route path="/overview" element={<Overview />} />
+                  <Route index element={<Overview />} />
+                  <Route path="/applications" element={<Applications />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/employee" element={<Employee />} />
+                  <Route path="/meetings" element={<Meeting />} />
+                  <Route path="/feed" element={<Feed />} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/live-video" element={<RTC />} />
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
+        </WorkspaceProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
