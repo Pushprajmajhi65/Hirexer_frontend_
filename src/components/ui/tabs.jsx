72cb037a93420094c8b455b457c650b09ca12add
Pushprajmajhi65 <pushprajmajhi65@gmail.com>
2025-03-29 08:54:24 +0545

    added components from shadcn

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
@@ -25,7 +23,7 @@ function TabsList({
     (<TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        " text-muted-foreground inline-flex h-9 space-x-4 w-fit items-center justify-center rounded-lg p-[3px]",
         className
       )}
       {...props} />)
@@ -40,7 +38,7 @@ function TabsTrigger({
     (<TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "inline-flex items-center justify-start whitespace-nowrap rounded-md py-1 text-[20px] font-[700] decoration-2 underline-offset-[11px] ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-primary/90 data-[state=active]:after:w-[90%] hover:text-figmaPrimary relative after:absolute after:left-1/2 after:bottom-0.5 after:h-0.5 after:w-0 after:bg-figmaPrimary after:transition-all after:duration-300 after:translate-x-[-50%] hover:after:w-[95%]",
         className
       )}
       {...props} />)
