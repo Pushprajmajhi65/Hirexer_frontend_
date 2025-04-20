150ac99dcb8a96d896e520de02ce791a34f66bd9
Pushprajmajhi65 <145209230+Pushprajmajhi65@users.noreply.github.com>
2025-04-21 02:11:25 +0545

    env setup

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
-export const BACKEND_URL = "http://127.0.0.1:8000/";
+export const BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
+// export const BACKEND_URL = "http://127.0.0.1:8000/";
 export const WEATHER_API_KEY=import.meta.env.VITE_WEATHER_API_KEY
 export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL || BACKEND_URL.replace(/^http/, 'ws');
